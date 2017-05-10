af63a3e8af713d2146569492b80f932cec3ffecd
Ali Ergal <ergal27@gmail.com>
2017-05-10 16:10:01 +0100

    First Commit

---
+++ curl.php
@@ -0,0 +1,108 @@
+<?php
+require_once 'response.php';
+class Curl {
+    
+    public $follow_redirects = true;
+    public $headers = array();
+    public $options = array();
+    public $referer;
+    public $user_agent;
+    public $body_type;
+    public $url;
+
+    protected $error = '';
+    protected $request;
+
+    function __construct() {
+        $this->user_agent = isset($_SERVER['HTTP_USER_AGENT']) ? $_SERVER['HTTP_USER_AGENT'] : 'Curl/PHP '.PHP_VERSION.' (http://atom42.co.uk)';
+    }
+    
+    function delete($vars = array()) {
+        return $this->request('DELETE', $vars);
+    }
+    
+    function error() {
+        return $this->error;
+    }
+    
+    function get($vars = array()) {
+        if (!empty($vars)) {
+            $this->url .= (stripos($url, '?') !== false) ? '&' : '?';
+            $this->url .= (is_string($vars)) ? $vars : http_build_query($vars, '', '&');
+        }
+        return $this->request('GET');
+    }
+    
+    function head($vars = array()) {
+        return $this->request('HEAD', $vars);
+    }
+    
+    function post($vars = array()) {
+        return $this->request('POST', $vars);
+    }
+    
+    function put($vars = array()) {
+        return $this->request('PUT', $vars);
+    }
+    
+    function request($method, $vars = array()) {
+        $this->error = '';
+        $this->request = curl_init();
+        
+        $this->set_request_method($method);
+        $this->set_request_options($vars);
+        $this->set_request_headers();
+        
+        $response = curl_exec($this->request);
+        
+        if ($response) {
+            $response = new CurlResponse($response);
+        } else {
+            $this->error = curl_errno($this->request).' - '.curl_error($this->request);
+        }
+        
+        curl_close($this->request);
+        
+        return $response;
+    }
+    
+    protected function set_request_headers() {
+        $headers = array();
+        foreach ($this->headers as $key => $value) {
+            $headers[] = $key.': '.$value;
+        }
+        curl_setopt($this->request, CURLOPT_HTTPHEADER, $headers);
+    }
+    
+    protected function set_request_method($method) {
+        switch (strtoupper($method)) {
+            case 'HEAD':
+                curl_setopt($this->request, CURLOPT_NOBODY, true);
+                break;
+            case 'GET':
+                curl_setopt($this->request, CURLOPT_HTTPGET, true);
+                break;
+            case 'POST':
+                curl_setopt($this->request, CURLOPT_POST, true);
+                break;
+            default:
+                curl_setopt($this->request, CURLOPT_CUSTOMREQUEST, $method);
+        }
+    }
+    
+    protected function set_request_options($vars) {
+        curl_setopt($this->request, CURLOPT_URL, $this->url);
+        if (!empty($vars)) curl_setopt($this->request, CURLOPT_POSTFIELDS, $vars);
+        
+        curl_setopt($this->request, CURLOPT_HEADER, true);
+        curl_setopt($this->request, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($this->request, CURLOPT_USERAGENT, $this->user_agent);
+
+        if ($this->follow_redirects) curl_setopt($this->request, CURLOPT_FOLLOWLOCATION, true);
+        if ($this->referer) curl_setopt($this->request, CURLOPT_REFERER, $this->referer);
+        
+        foreach ($this->options as $option => $value) {
+            curl_setopt($this->request, constant('CURLOPT_'.str_replace('CURLOPT_', '', strtoupper($option))), $value);
+        }
+    }
+}
---
+++ response.php
@@ -0,0 +1,32 @@
+<?php
+class CurlResponse {
+    
+    public $body = '';
+    public $headers = array();
+    
+    function __construct($response) {
+        $pattern = '#HTTP/\d\.\d.*?$.*?\r\n\r\n#ims';
+        
+        preg_match_all($pattern, $response, $matches);
+        $headers_string = array_pop($matches[0]);
+        $headers = explode("\r\n", str_replace("\r\n\r\n", '', $headers_string));
+        
+        $this->body = str_replace($headers_string, '', $response);
+        
+        $version_and_status = array_shift($headers);
+        preg_match('#HTTP/(\d\.\d)\s(\d\d\d)\s(.*)#', $version_and_status, $matches);
+        $this->headers['Http-Version'] = $matches[1];
+        $this->headers['Status-Code'] = $matches[2];
+        $this->headers['Status'] = $matches[2].' '.$matches[3];
+        
+        foreach ($headers as $header) {
+            preg_match('#(.*?)\:\s(.*)#', $header, $matches);
+            $this->headers[$matches[1]] = $matches[2];
+        }
+    }
+    
+    function __toString() {
+        return $this->body;
+    }
+    
+}
